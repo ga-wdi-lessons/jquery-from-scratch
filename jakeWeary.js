fa3436e2b199c24bcb0dfb3e1e4e2a43eeb5f16b
Jesse Shawl <jesse@jshawl.com>
2016-06-06 10:44:57 -0400

    add html

--- jakeWeary.js
+++ jakeWeary.js
@@ -6,15 +6,20 @@ var $ = (function(){
       var els = document.querySelectorAll(selector)
     }
     els.on = function(eventName, callback){
-      for(var i = 0; i < els.length; i ++){
+      for(var i = 0; i < els.length; i++){
 	els[i].addEventListener(eventName, callback)
       }
     }
     els.css = function(props){
-      for(var i = 0; i < els.length; i ++){
+      for(var i = 0; i < els.length; i++){
         for(var key in props){
 	  els[i].style[key] = props[key]
 	}
+      }
+    }
+    els.html = function(){
+      for(var i = 0; i < els.length; i++){
+        return els[i].innerHTML 
       }
     }
     return els
@@ -22,9 +27,16 @@ var $ = (function(){
 
   return jakeWeary
 
+  function each(collection, callback){
+    for(var i = 0; i < collection.length; i++){
+      callback(collection[i])
+    }
+  }
+
 })()
 
 
 $(".box").on("click", function(){
   $(this).css({'background':'red'})
+  console.log($(this).html())
 })
